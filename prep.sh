93e763134eef45b0e1cff8c096e8c7f4fd98ec75
Erik Nelson <erik@nsk.io>
2021-07-08 16:48:35 -0400

    Update so get prep script working

--- prep.sh
+++ prep.sh
@@ -6,9 +6,9 @@ mkdir -p $BUILD_DIR
 mkdir -p $BIN_DIR
 mkdir -p $PLUGIN_OUT_DIR
 
-#git clone --depth=1 https://github.com/konveyor/crane.git $BUILD_DIR/crane
-#cd $BUILD_DIR/crane
-#go build -o $BIN_DIR/crane
+git clone --depth=1 https://github.com/konveyor/crane.git $BUILD_DIR/crane
+cd $BUILD_DIR/crane
+go build -o $BIN_DIR/crane
 
 plugins=$(ls -1 $_dir/plugins)
 shopt -s nullglob
@@ -16,7 +16,7 @@ plugins=($_dir/plugins/*/)
 shopt -u nullglob
 for p in "${plugins[@]}"; do
   pluginMain="$(ls $p)"
-  cd $p
+  pushd $p
   go build -o $PLUGIN_OUT_DIR $pluginMain
   popd
 done
