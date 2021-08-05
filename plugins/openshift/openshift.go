3b0e51918a19c4fdcdaf426223c53817827f2cf5
Erik Nelson <erik@nsk.io>
2021-08-05 14:44:29 -0400

    Update and commit up-to-date plugins
    Most importantly, they're opt arg compliant

--- plugins/openshift/openshift.go
+++ plugins/openshift/openshift.go
@@ -6,46 +6,11 @@ import (
 	"strings"
 
 	jsonpatch "github.com/evanphx/json-patch"
-	"github.com/konveyor/crane-lib/transform"
-	"github.com/konveyor/crane-lib/transform/cli"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 var defaultPullSecrets = []string{"builder-dockercfg-", "default-dockercfg-", "deployer-dockercfg-"}
-
-func main() {
-	// TODO: add plumbing for logger in the cli-library and instantiate here
-	// TODO: add plumbing for passing flags in the cli-library
-	u, err := cli.Unstructured(cli.ObjectReaderOrDie())
-	if err != nil {
-		cli.WriterErrorAndExit(fmt.Errorf("error getting unstructured object: %#v", err))
-	}
-
-	cli.RunAndExit(cli.NewCustomPlugin("OpenshiftCustomPlugin", Run), u)
-}
-
-func Run(u *unstructured.Unstructured) (transform.PluginResponse, error) {
-	// plugin writers need to write custome code here.
-	var patch jsonpatch.Patch
-	var err error
-	switch u.GetKind() {
-	case "Pod":
-		patch, err = UpdateDefaultPullSecrets(*u)
-	case "Route":
-		patch, err = UpdateRoute(*u)
-	case "Service":
-		patch, err = UpdateService(*u)
-	}
-	if err != nil {
-		return transform.PluginResponse{}, err
-	}
-	return transform.PluginResponse{
-		Version:    "v1",
-		IsWhiteOut: false,
-		Patches:    patch,
-	}, nil
-}
 
 func UpdateDefaultPullSecrets(u unstructured.Unstructured) (jsonpatch.Patch, error) {
 	pullSecrets := getPullSecrets(u)
@@ -67,6 +32,45 @@ func UpdateDefaultPullSecrets(u unstructured.Unstructured) (jsonpatch.Patch, err
 		}
 	}
 
+	return jsonPatch, nil
+}
+
+func UpdateServiceAccount(u unstructured.Unstructured) (jsonpatch.Patch, error) {
+	pullSecrets := getPullSecretReferencesServiceAccount(u)
+
+	jsonPatch := jsonpatch.Patch{}
+
+	check := u.GetName() + "-dockercfg-"
+
+	for n, secret := range pullSecrets {
+		if strings.HasPrefix(secret.Name, check) {
+
+			patchJSON := fmt.Sprintf(`[
+{ "op": "remove", "path": "/imagePullSecrets/%v"}
+]`, n)
+			patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+			if err != nil {
+				return nil, err
+			}
+			jsonPatch = append(jsonPatch, patch...)
+
+		}
+	}
+
+	secrets := getSecretReferencesServiceAccount(u)
+	for n, secret := range secrets {
+		if strings.HasPrefix(secret.Name, check) {
+
+			patchJSON := fmt.Sprintf(`[
+{ "op": "remove", "path": "/secrets/%v"}
+]`, n)
+			patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+			if err != nil {
+				return nil, err
+			}
+			jsonPatch = append(jsonPatch, patch...)
+		}
+	}
 	return jsonPatch, nil
 }
 
@@ -107,15 +111,34 @@ func getPullSecrets(u unstructured.Unstructured) []v1.LocalObjectReference {
 	return pod.Spec.ImagePullSecrets
 }
 
-func UpdateService(u unstructured.Unstructured) (jsonpatch.Patch, error) {
-	patchJSON := fmt.Sprintf(`[
-{ "op": "remove", "path": "/spec/clusterIPs"}, 
-{ "op": "remove", "path": "/spec/clusterIP"}
-]`)
-
-	patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+func getPullSecretReferencesServiceAccount(u unstructured.Unstructured) []v1.LocalObjectReference {
+	js, err := u.MarshalJSON()
 	if err != nil {
-		return nil, err
+		return nil
 	}
-	return patch, nil
+
+	sa := &v1.ServiceAccount{}
+
+	err = json.Unmarshal(js, sa)
+	if err != nil {
+		return nil
+	}
+
+	return sa.ImagePullSecrets
+}
+
+func getSecretReferencesServiceAccount(u unstructured.Unstructured) []v1.ObjectReference {
+	js, err := u.MarshalJSON()
+	if err != nil {
+		return nil
+	}
+
+	sa := &v1.ServiceAccount{}
+
+	err = json.Unmarshal(js, sa)
+	if err != nil {
+		return nil
+	}
+
+	return sa.Secrets
 }
